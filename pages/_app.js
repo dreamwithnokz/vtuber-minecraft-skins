0eac42f26e52d7f46ad53bb1eefeae986393af4c
Nokz <dreamwithnokz@gmail.com>
2021-03-07 11:51:17 +0800

    Revert "fouc fix"
    This reverts commit 6f0f609c75adaaf2aa1f3fe889b6241a16be7f5d.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,30 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <script>0</script>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
-
-export default MyDocument;
